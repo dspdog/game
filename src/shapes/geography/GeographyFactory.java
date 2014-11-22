95f0ab6c7814e1fc64bf365160156557f8eed2ea
mrpawel <pawel745@gmail.com>
2014-11-22 02:20:30 -0500

    pretty terrain

--- src/shapes/geography/GeographyFactory.java
+++ src/shapes/geography/GeographyFactory.java
@@ -7,6 +7,12 @@ import utils.SimplexNoise;
  */
 public class GeographyFactory {
     public static  float geographyFunction(float x, float y){
-        return  (float)(1f-(SimplexNoise.noise(x / 20f, y / 20f)+1f)*(SimplexNoise.noise(x/20f,y/20f)+1f))*10f;
+        float total = 0;
+
+        total+=SimplexNoise.noise(x / 10f, y / 10f)*3f;
+        total+=SimplexNoise.noise(x / 20f, y / 20f)*10f;
+        total+=SimplexNoise.noise(x / 80f, y / 80f)*10f;
+        total+=SimplexNoise.noise(x / 160f, y / 160f)*20f;
+        return (total+20f)/2f;
     }
 }
