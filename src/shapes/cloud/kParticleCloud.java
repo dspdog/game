105ed15494997671fd70a0667891b3e35949f098
mrpawel <pawel745@gmail.com>
2014-12-13 21:59:58 -0500

    ...

--- src/shapes/cloud/kParticleCloud.java
+++ src/shapes/cloud/kParticleCloud.java
@@ -6,26 +6,26 @@ import com.amd.aparapi.Range;
  */
 public class kParticleCloud extends Kernel {
     static final int PARTICLES_MAX = 100000;
-    static int numParticles=0;
+    int numParticles=0;
 
     //PARTICLE PARAMS
-    static final float[] vx = new float[PARTICLES_MAX]; //velocity_x
-    static final float[] vy = new float[PARTICLES_MAX]; //velocity_y
-    static final float[] vz = new float[PARTICLES_MAX]; //velocity_z
+    final float[] vx = new float[PARTICLES_MAX]; //velocity_x
+    final float[] vy = new float[PARTICLES_MAX]; //velocity_y
+    final float[] vz = new float[PARTICLES_MAX]; //velocity_z
 
-    static final float[] px = new float[PARTICLES_MAX]; //position_x
-    static final float[] py = new float[PARTICLES_MAX]; //position_y
-    static final float[] pz = new float[PARTICLES_MAX]; //position_z
+    final float[] px = new float[PARTICLES_MAX]; //position_x
+    final float[] py = new float[PARTICLES_MAX]; //position_y
+    final float[] pz = new float[PARTICLES_MAX]; //position_z
 
-    static final float[] pd = new float[PARTICLES_MAX]; //density
-    static final float[] pm = new float[PARTICLES_MAX]; //mass
-    static final float[] pp = new float[PARTICLES_MAX]; //pressure
+    final float[] pd = new float[PARTICLES_MAX]; //density
+    final float[] pm = new float[PARTICLES_MAX]; //mass
+    final float[] pp = new float[PARTICLES_MAX]; //pressure
 
-    static final int MAX_NEIGHBORS = 20;
-    static final int[] pn = new int[PARTICLES_MAX*MAX_NEIGHBORS]; //neighbors by index
-    static final int[] pnn = new int[PARTICLES_MAX]; //neighbors totals by index
+    final int MAX_NEIGHBORS = 20;
+    final int[] pn = new int[PARTICLES_MAX*MAX_NEIGHBORS]; //neighbors by index
+    final int[] pnn = new int[PARTICLES_MAX]; //neighbors totals by index
 
-    static final float neighborDistance = 1.0f;
+    final float neighborDistance = 1.0f;
 
     public kParticleCloud(int _numParticles){
         numParticles=min(_numParticles, PARTICLES_MAX);
@@ -74,15 +74,6 @@ public class kParticleCloud extends Kernel {
         return false;
     }
 
-    public void findNeighbors(int i){
-        resetNeighbors(i);
-        for(int o=0; o<numParticles; o++){
-            if(distance(o,i)<neighborDistance){
-                addNeighbor(i,o);
-            }
-        }
-    }
-
     public float distance(int i, int o){
         float x = px[i]-px[o];
         float y = py[i]-py[o];
@@ -113,10 +104,25 @@ public class kParticleCloud extends Kernel {
 
     @Override
     public void run() {
-        //findNeighbors
-        //updateParticleVelocities();
-        //updateParticlePositions();
         int i = getGlobalId(0);
         int pass = getPassId();
+
+        if(pass==0){
+            findNeighbors(i);
+        }else if(pass==1){
+            //updateParticleVelocities(i);
+            //updateParticlePositions();
+        }
     }
+
+
+    public void findNeighbors(int i){
+        resetNeighbors(i);
+        for(int o=0; o<numParticles; o++){
+            if(distance(o,i)<neighborDistance){
+                addNeighbor(i,o);
+            }
+        }
+    }
+
 }
