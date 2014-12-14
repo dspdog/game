b34bb6584ca61e4db2c32fc99ba01c91cf4e79dc
mrpawel <pawel745@gmail.com>
2014-12-13 20:18:40 -0500

    flattening it outtttt

--- src/shapes/cloud/kParticleCloud.java
+++ src/shapes/cloud/kParticleCloud.java
@@ -8,7 +8,18 @@ public class kParticleCloud extends Kernel {
     static final int PARTICLES_MAX = 100000;
     static int numParticles=0;
 
-    static final kParticle[] theParticles = new kParticle[PARTICLES_MAX];
+    //PARTICLE PARAMS
+    static final float[] vx = new float[PARTICLES_MAX]; //velocity_x
+    static final float[] vy = new float[PARTICLES_MAX]; //velocity_y
+    static final float[] vz = new float[PARTICLES_MAX]; //velocity_z
+
+    static final float[] px = new float[PARTICLES_MAX]; //position_x
+    static final float[] py = new float[PARTICLES_MAX]; //position_y
+    static final float[] pz = new float[PARTICLES_MAX]; //position_z
+
+    static final float[] pd = new float[PARTICLES_MAX]; //density
+    static final float[] pm = new float[PARTICLES_MAX]; //mass
+    static final float[] pp = new float[PARTICLES_MAX]; //pressure
 
     static final float neighborDistance = 1.0f;
 
@@ -19,7 +30,7 @@ public class kParticleCloud extends Kernel {
 
     public void generateParticles(){
         for(int i=0; i<numParticles; i++){
-            theParticles[i] = new kParticle();
+            //theParticles[i] = new particle();
         }
     }
 
@@ -31,7 +42,6 @@ public class kParticleCloud extends Kernel {
 
         this.setExplicit(true);
 
-
         //update data w/:
         //this.put(lineXY1).put(lineZS1).put(lineDI)
         //        .put(lineXY2).put(lineZS2);
@@ -41,6 +51,7 @@ public class kParticleCloud extends Kernel {
         this.execute(range, 3);
         //this.get(pixels);
 
+        System.out.println(this.getExecutionMode() + " " + this.getExecutionTime());
         System.out.println("ran in " + (System.currentTimeMillis()-time1)+"ms " + numParticles);
     }
 
@@ -52,32 +63,9 @@ public class kParticleCloud extends Kernel {
 
         int i = getGlobalId(0);
         int pass = getPassId();
-        kParticle theParticle = theParticles[i];
 
-        switch (pass){
-            case 0:
-                findNeighbors(theParticle);
-                break;
-            case 1:
-                break;
-            case 2:
-                break;
-        }
-    }
 
-    public void findNeighbors(kParticle theParticle){
-        theParticle.clearNeighbors();
-        for(int i=0; i<numParticles; i++){
-            if(distance(theParticle, theParticles[i])<neighborDistance){
-                theParticle.addNeighbor(i);
-            }
-        }
-    }
+        //particle theParticle = theParticles[i];
 
-    public float distance(kParticle p1, kParticle p2){
-        float dx = p1.px - p2.px;
-        float dy = p1.py - p2.py;
-        float dz = p1.pz - p2.pz;
-        return sqrt(dx*dx + dy*dy + dz*dz);
     }
 }
